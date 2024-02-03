f8da1e8061fb5c379e364740cc69069819644dbc
Bruno Bettio <bruno.web.bpage@gmail.com>
2024-02-03 17:43:24 -0300

    enrutamiento y estructura base

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,91 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+    const activeStyle = 'underline underline-offset-4';
+
+  return (
+    <nav className="flex justify-between items-center p-4 fixed z-10 w-full py-5 px-8 text-sm font-light top-0">
+        <ul className="flex items-center gap-6">
+            <li className="font-semibold text-lg">
+                <NavLink 
+                to='/'>
+                Shopi
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to='/'
+                className={({isActive}) => isActive ? activeStyle : undefined}
+                >
+                    All
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to='/Electronics'
+                className={({isActive}) => isActive ? activeStyle : undefined}
+                >
+                    Electronics
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to='/furnitures'
+                className={({isActive}) => isActive ? activeStyle : undefined}
+                >
+                Furnitures
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to='/toys'
+                className={({isActive}) => isActive ? activeStyle : undefined}
+                >
+                    Toys
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to='/others'
+                className={({isActive}) => isActive ? activeStyle : undefined}
+                >
+                    Others
+                </NavLink>
+            </li>
+        </ul>
+        <ul className="flex items-center gap-6">
+            <li className="text-black/60">
+                bruno@correo.com
+            </li>
+            <li>
+                <NavLink 
+                to='/my-orders'
+                className={({isActive}) => isActive ? activeStyle : undefined}
+                >
+                    My Orders
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to='/my-account'
+                className={({isActive}) => isActive ? activeStyle : undefined}>
+                    My Account
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                to='/sign-in'
+                className={({isActive}) => isActive ? activeStyle : undefined}
+                >
+                    Sign In
+                </NavLink>
+            </li>
+            <li>
+                🛒 0
+            </li>
+        </ul>
+    </nav>        
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
+
+function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, []);
+
+  return (
+    <Layout>
+      Home
+      <div className="grid grid-cols-4 w-full max-w-screen-lg gap-4">
+      {
+        items?.map(item => (
+         <Card key={item.id} data={item}/>
+        ))
+      }
+      </div>
+    </Layout>
+  )
+}
+
+export default Home;
